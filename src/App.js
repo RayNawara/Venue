db2f143d9ad4bbf005334d6dfe6450b62c3e02a1
Raymond Nawara <rjnawara@gmail.com>
2019-07-02 12:58:29 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Element } from 'react-scroll'
+import './resources/styles.css'
+import Header from './components/header-footer/Header'
+import Featured from './components/featured'
+import VenueNfo from './components/venueNfo'
+import Highlights from './components/Highlights'
+import Pricing from './components/pricing'
+import Location from './components/location'
+import Footer from './components/header-footer/Footer'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App" style={{ height: "800px", background: "cornflowerblue" }}>
+        <Header/>
+        <Element name="event"><Featured/></Element>
+        <Element name="venue"><VenueNfo/></Element>
+        <Element name="highlights"><Highlights/></Element>
+        <Element name="pricing"><Pricing/></Element>
+        <Element name="location"><Location/></Element>
+        <Footer/>
+      </div>
+    )
+  }
 }
 
 export default App;
